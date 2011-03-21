0c388927913b05134be54309e6693ca8806e1d4d
Eduard Kracmar <kracmar@dannax.sk>
2011-03-21 13:12:57 +0100

    Added UTF-8 charset to headers

--- GettextExtractor.php
+++ GettextExtractor.php
@@ -57,7 +57,7 @@ class GettextExtractor {
 		"Last-Translator" => "FULL NAME <EMAIL@ADDRESS>",
 		"Language-Team" => "LANGUAGE <LL@li.org>",
 		"MIME-Version" => "1.0",
-		"Content-Type" => "text/plain; charset=CHARSET",
+		"Content-Type" => "text/plain; charset=UTF-8",
 		"Content-Transfer-Encoding" => "8bit",
 		"Plural-Forms" => "nplurals=INTEGER; plural=EXPRESSION;"
 	);
