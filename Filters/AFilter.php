791c38e66f4629672a7b0f9eb3ecbdfceb8b3e3c
Eduard Kracmar <kracmar@dannax.sk>
2011-03-22 11:11:30 +0100

    Formatting

--- Filters/AFilter.php
+++ Filters/AFilter.php
@@ -13,7 +13,6 @@
  * @license    New BSD License
  * @package    Nette Extras
  */
-
 require_once dirname(__FILE__) . '/iFilter.php';
 
 /**
@@ -22,68 +21,68 @@ require_once dirname(__FILE__) . '/iFilter.php';
  * @author Ondřej Vodáček
  */
 abstract class AFilter {
-	
+
     /** @var array */
     protected $functions = array();
 
     /**
      * Includes a function to parse gettext phrases from
-	 *
+     *
      * @param $functionName string
      * @param $singular int
-	 * @param $plural int|null
-	 * @param $context int|null
-	 * @return AFilter
+     * @param $plural int|null
+     * @param $context int|null
+     * @return AFilter
      */
-	public function addFunction($functionName, $singular = 1, $plural = null, $context = null) {
-		if (!is_int($singular) || $singular <= 0) {
-			throw new InvalidArgumentException('Invalid argument type or value given for paramater $singular.');
-		}
+    public function addFunction($functionName, $singular = 1, $plural = null, $context = null) {
+        if (!is_int($singular) || $singular <= 0) {
+            throw new InvalidArgumentException('Invalid argument type or value given for paramater $singular.');
+        }
         $function = array(
-			$singular => iFilter::SINGULAR
-		);
-		if ($plural !== null) {
-			if (!is_int($plural) || $plural <= 0) {
-				throw new InvalidArgumentException('Invalid argument type or value given for paramater $plural.');
-			}
-			$function[$plural] = iFilter::PLURAL;
-		}
-		if ($context !== null) {
-			if (!is_int($context) || $context <= 0) {
-				throw new InvalidArgumentException('Invalid argument type or value given for paramater $context.');
-			}
-			$function[$context] = iFilter::CONTEXT;
-		}
-		$this->functions[$functionName] = $function;
-		return $this;
+            $singular => iFilter::SINGULAR
+        );
+        if ($plural !== null) {
+            if (!is_int($plural) || $plural <= 0) {
+                throw new InvalidArgumentException('Invalid argument type or value given for paramater $plural.');
+            }
+            $function[$plural] = iFilter::PLURAL;
+        }
+        if ($context !== null) {
+            if (!is_int($context) || $context <= 0) {
+                throw new InvalidArgumentException('Invalid argument type or value given for paramater $context.');
+            }
+            $function[$context] = iFilter::CONTEXT;
+        }
+        $this->functions[$functionName] = $function;
+        return $this;
     }
 
     /**
      * Excludes a function from the function list
-	 *
+     *
      * @param $functionName
-	 * @return AFilter
+     * @return AFilter
      */
     public function removeFunction($functionName) {
         unset($this->functions[$functionName]);
-		return $this;
+        return $this;
     }
 
     /**
      * Excludes all functions from the function list
-	 *
-	 * @return AFilter
+     *
+     * @return AFilter
      */
     public function removeAllFunctions() {
         $this->functions = array();
-		return $this;
+        return $this;
     }
 
     /**
      * Removes backslashes from before primes and double primes in primed or double primed strings respectively
-	 *
+     *
      * @return string
-	 * @author Matěj Humpál (https://github.com/finwe)
+     * @author Matěj Humpál (https://github.com/finwe)
      */
     protected function fixEscaping($string) {
         $prime = substr($string, 0, 1);
@@ -92,21 +91,22 @@ abstract class AFilter {
         return $string;
     }
 
-	/**
-	 * Remove single or double quotes from begin and end of the string.
-	 *
-	 * @param string $string
-	 * @return string
-	 */
-	protected function stripQuotes($string) {
+    /**
+     * Remove single or double quotes from begin and end of the string.
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function stripQuotes($string) {
         $prime = substr($string, 0, 1);
-		if ($prime === "'" || $prime === '"') {
-			if (substr($string, -1, 1) === $prime) {
-				$string = substr($string, 1, -1);
-			}
-		}
-		return $string;
-	}
-    
+        if ($prime === "'" || $prime === '"') {
+            if (substr($string, -1, 1) === $prime) {
+                $string = substr($string, 1, -1);
+            }
+        }
+        return $string;
+    }
+
 }
+
 ?>
